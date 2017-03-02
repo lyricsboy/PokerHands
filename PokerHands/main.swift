834460ac6446084743709e20dd1c666ed36d6f9d
Brian Hardy <lyricsboy@users.noreply.github.com>
2017-03-01 19:10:42 -0500

    Initial Commit

---
+++ PokerHands/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PokerHands
+//
+//  Created by Brian Hardy on 3/1/17.
+//  Copyright © 2017 Kata Corp. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
