f7effec041f01de4ebf7a0cb66b6e891efc3268e
Brian Hardy <lyricsboy@users.noreply.github.com>
2017-03-02 22:52:38 -0500

    Initial Commit

---
+++ Poker/PokerHand.swift
@@ -0,0 +1,173 @@
+//
+//  PokerHand.swift
+//  PokerHands
+//
+//  Created by Brian Hardy on 3/1/17.
+//  Copyright © 2017 Kata Corp. All rights reserved.
+//
+
+import Foundation
+
+enum Suit: String {
+    case heart = "H"
+    case club = "C"
+    case diamond = "D"
+    case spade = "S"
+}
+
+enum Rank: Int {
+    case two = 2
+    case three
+    case four
+    case five
+    case six
+    case seven
+    case eight
+    case nine
+    case ten
+    case jack
+    case queen
+    case king
+    case ace
+    
+    static func from(string: String) -> Rank? {
+        switch string {
+        case "2"..."9":
+            guard let intValue = Int(string) else { return nil}
+            return Rank(rawValue: intValue)
+        case "0": return .ten
+        case "J": return .jack
+        case "Q": return .queen
+        case "K": return .king
+        case "A": return .ace
+        default: return nil
+        }
+    }
+    
+}
+
+extension Rank: Comparable { }
+
+func <(lhs: Rank, rhs: Rank) -> Bool {
+    return lhs.rawValue < rhs.rawValue
+}
+
+enum KnownHand: Equatable {
+    case highCard(Rank)
+    case pair(Rank)
+    case twoPair(Rank, Rank)
+    case threeOfAKind(Rank)
+    case straight(Rank)
+    case flush(Suit, Rank)
+    case fullHouse(threeOf: Rank, twoOf: Rank)
+    case fourOfAKind(Rank)
+    case straightFlush(Rank)
+    
+    static func from(pokerHand: PokerHand) -> KnownHand {
+        // prefer the best hand
+        
+        let cardsSortedByRank = pokerHand.cards.sorted {
+            $0.rank > $1.rank
+        }
+        // how many cards are there for each rank?
+        let cardsGroupedByRank = pokerHand.cardsGroupedByRank()
+        
+        // pairs
+        let pairs = cardsGroupedByRank.sorted { (leftRankCards, rightRankCards) -> Bool in
+            return leftRankCards.value.count > rightRankCards.value.count
+        }.filter { (rankCards) -> Bool in
+            rankCards.value.count == 2
+        }
+        if pairs.count == 2 {
+            return .twoPair(pairs[0].key, pairs[1].key)
+        } else if pairs.count == 1 {
+            return .pair(pairs[0].key)
+        }
+
+        return .highCard(cardsSortedByRank[0].rank)
+    }
+}
+
+func ==(lhs: KnownHand, rhs: KnownHand) -> Bool {
+    switch (lhs, rhs) {
+    case (.twoPair(let lrank1, let lrank2), .twoPair(let rrank1, let rrank2)):
+        return lrank1 == rrank1 && lrank2 == rrank2
+    case (.pair(let lrank), .pair(let rrank)):
+        return lrank == rrank
+    case (.highCard(let lrank), .highCard(let rrank)):
+        return lrank == rrank
+    // TODO: The rest of them
+    default:
+        return false
+    }
+}
+
+struct Card: Equatable {
+    let suit: Suit
+    let rank: Rank
+    
+    init?(string: String) {
+        guard string.characters.count == 2 else {
+            return nil
+        }
+        guard let possibleRank = string.characters.first, let possibleSuit = string.characters.dropFirst().first else {
+            return nil
+        }
+        guard let rank = Rank.from(string: String(possibleRank)) else {
+            return nil
+        }
+        guard let suit = Suit(rawValue: String(possibleSuit)) else {
+            return nil
+        }
+        self.suit = suit
+        self.rank = rank
+    }
+    
+    init(suit: Suit, rank: Rank) {
+        self.suit = suit
+        self.rank = rank
+    }
+}
+
+func ==(lhs: Card, rhs: Card) -> Bool {
+    return lhs.suit == rhs.suit && lhs.rank == rhs.rank
+}
+
+extension Card: Hashable {
+    
+    var hashValue: Int {
+        return suit.hashValue ^ rank.hashValue
+    }
+}
+
+
+struct PokerHand {
+    
+    let cards: [Card]
+    
+    init?(string: String) {
+        let stringCards = string.components(separatedBy: " ")
+        guard stringCards.count == 5 else {
+            return nil
+        }
+        let validCards = stringCards.flatMap {
+            Card(string: $0)
+        }
+        // only a five-card hand of non-duplicates is valid
+        guard Array(Set<Card>(validCards)).count == 5 else {
+            return nil
+        }
+        self.cards = validCards
+    }
+    
+    func cardsGroupedByRank() -> [Rank: [Card]] {
+        var cardsByRank: [Rank: [Card]] = [:]
+        for card in cards {
+            var existingCards = cardsByRank[card.rank] ?? []
+            existingCards.append(card)
+            cardsByRank[card.rank] = existingCards
+        }
+        return cardsByRank
+    }
+    
+}
---
+++ PokerTests/PokerTests.swift
@@ -0,0 +1,121 @@
+//
+//  PokerTests.swift
+//  PokerTests
+//
+//  Created by Brian Hardy on 3/1/17.
+//  Copyright © 2017 Kata Corp. All rights reserved.
+//
+
+import XCTest
+@testable import Poker
+
+class PokerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testCreateHandFromString() {
+        // define a string hand
+        let validHand = "4H 4S 4D 9S 9D"
+        // try to create a PokerHand from that string
+        let hand = PokerHand(string: validHand)
+        // verify that it has the right cards
+        XCTAssertNotNil(hand)
+        XCTAssertEqual(hand?.cards.count, 5)
+        
+        let expectedCards: [Card] = [
+            Card(suit: .heart, rank: .four),
+            Card(suit: .spade, rank: .four),
+            Card(suit: .diamond, rank: .four),
+            Card(suit: .spade, rank: .nine),
+            Card(suit: .diamond, rank: .nine)
+        ]
+        XCTAssertEqual(hand!.cards, expectedCards)
+    }
+    
+    func testCreateHandFromInvalidString() {
+        var invalidHand = ""
+        
+        var hand = PokerHand(string: invalidHand)
+        XCTAssertNil(hand)
+        
+        invalidHand = "100J 2H 5K 2Q ZX"
+        hand = PokerHand(string: invalidHand)
+        XCTAssertNil(hand)
+        
+        invalidHand = "JH JH 4H AH KH" // two of the same card (Jack of Hearts)
+        hand = PokerHand(string: invalidHand)
+        XCTAssertNil(hand)
+    }
+    
+    func testCreateCardFromString() {
+        let suits: [String: Suit] = [
+            "S": .spade,
+            "H": .heart,
+            "C": .club,
+            "D": .diamond
+        ]
+        let ranks: [String: Rank] = [
+            "2": .two,
+            "3": .three,
+            "4": .four,
+            "5": .five,
+            "6": .six,
+            "7": .seven,
+            "8": .eight,
+            "9": .nine,
+            "0": .ten,
+            "J": .jack,
+            "Q": .queen,
+            "K": .king,
+            "A": .ace
+        ]
+        for suit in suits {
+            for rank in ranks {
+                let actualCard = Card(string: rank.key+suit.key)
+                let expectedCard = Card(suit: suit.value, rank: rank.value)
+                XCTAssertEqual(actualCard, expectedCard)
+            }
+        }
+    }
+    
+    func testHighCard() {
+        let aceHigh = PokerHand(string: "4H 3D QD JD AH")
+        XCTAssertNotNil(aceHigh)
+        XCTAssertEqual(KnownHand.from(pokerHand: aceHigh!), .highCard(.ace))
+        
+        let sevenHigh = PokerHand(string: "4H 3D 5S 2S 7C")
+        XCTAssertNotNil(sevenHigh)
+        XCTAssertEqual(KnownHand.from(pokerHand: sevenHigh!), .highCard(.seven))
+    }
+    
+    func testPairs() {
+        let pairOfFours = "4H 4D QD JD AH"
+        
+        let pairOfFoursHand = PokerHand(string: pairOfFours)
+        XCTAssertNotNil(pairOfFoursHand)
+        
+        var knownHand = KnownHand.from(pokerHand: pairOfFoursHand!)
+        XCTAssertEqual(knownHand, KnownHand.pair(.four))
+        
+        let pairOfQueens = "4H QD JD QH KH"
+        let pairOfQueensHand = PokerHand(string: pairOfQueens)
+        XCTAssertNotNil(pairOfQueensHand)
+        knownHand = KnownHand.from(pokerHand: pairOfQueensHand!)
+        XCTAssertEqual(knownHand, KnownHand.pair(.queen))
+    }
+    
+    func testTwoPair() {
+        let twoPair = PokerHand(string: "4H 4C 2C AH AC")
+        XCTAssertNotNil(twoPair)
+        XCTAssertEqual(KnownHand.from(pokerHand: twoPair!), .twoPair(.four, .ace))
+    }
+    
+}
