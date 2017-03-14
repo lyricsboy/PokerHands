d1aab9d62121d6570c87d7cc1672d3fee05551fc
Brian Hardy <lyricsboy@users.noreply.github.com>
2017-03-14 13:49:12 -0400

    Passing testCreateCardFromString

--- PokerTests/PokerTests.swift
+++ PokerTests/PokerTests.swift
@@ -55,4 +55,34 @@ class PokerTests: XCTestCase {
         XCTAssertNil(hand)
     }
     
+    func testCreateCardFromString() {
+        let suits: [String: Suit] = [
+            "S": .spade,
+            "H": .heart,
+            "C": .club,
+            "D": .diamond
+        ]
+        let ranks: [String: Rank] = [
+            "2": .two,
+            "3": .three,
+            "4": .four,
+            "5": .five,
+            "6": .six,
+            "7": .seven,
+            "8": .eight,
+            "9": .nine,
+            "0": .ten,
+            "J": .jack,
+            "Q": .queen,
+            "K": .king,
+            "A": .ace
+        ]
+        for suit in suits {
+            for rank in ranks {
+                let actualCard = Card(string: rank.key+suit.key)
+                let expectedCard = Card(suit: suit.value, rank: rank.value)
+                XCTAssertEqual(actualCard, expectedCard)
+            }
+        }
+    }
 }
